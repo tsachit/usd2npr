12bfc697ff26a147f40f258589957227cdff1b33
Sachit Karki <sachitjungkarki@gmail.com>
2021-06-10 14:31:23 +0545

    converter added

--- README.md
+++ README.md
@@ -10,14 +10,14 @@ npm i -g electron-packager
 ```
 -> npm i -g electron-installer-dmg
 -> electron-packager . usd2npr --platform=darwin --arch=x64
--> electron-installer-dmg usd2npr-darwin-x64/usd2npr.app usd2npr
+-> electron-installer-dmg usd2npr-darwin-x64/usd2npr.app usd2npr_1.0.1_x64 --out=builds
 ```
 
 **For Linux(Debian), Install [electron-installer-debian](https://github.com/electron-userland/electron-installer-debian)**
 ```
 -> npm i -g electron-installer-debian
 -> electron-packager . usd2npr --platform=linux --arch=x64
--> electron-installer-debian --src usd2npr-linux-x64 --dest ./usd2npr --arch amd64
+-> electron-installer-debian --src usd2npr-linux-x64 --dest builds/ --arch amd64
 ```
 
 **For other platforms, click [here](https://github.com/electron/electron-packager#distributable-creators)**
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const cv = document.getElementById('currentValue');
+const usd = document.getElementById('usd');
+const npr = document.getElementById('npr');
+
 const fetchCurrentValue = async () => {
   const response = await fetch("https://api.transferwise.com/v3/comparisons?sourceCurrency=USD&targetCurrency=NPR&sendAmount=1", {
     method: 'GET',
@@ -8,12 +12,23 @@ const fetchCurrentValue = async () => {
 const roundTwo = (num) => parseFloat(num).toFixed(2).replace('.00', '');
 
 const getCurrentValue = () => {
-  const currentValue = document.getElementById('currentValue');
-  currentValue.innerHTML = '...';
+  cv.innerHTML = '...';
   fetchCurrentValue().then(json => {
     let result = json?.providers[0]?.quotes[0]?.rate;
-    currentValue.innerHTML = roundTwo(result);
+    cv.innerHTML = roundTwo(result);
+    convert();
   });
+};
+
+const convert = () => {
+  if(cv.innerHTML === '...') return;
+  if(usd.value === '') {
+    npr.innerHTML = '';
+    return;
+  }
+
+  const nprValue = roundTwo(cv.innerHTML) * roundTwo(usd.value);
+  npr.innerHTML = roundTwo(nprValue);
 };
 
 // run immediately
